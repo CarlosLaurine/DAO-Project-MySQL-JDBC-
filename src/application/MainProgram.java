f8f5b05731486f9c81e9488ee9928f60e89f9d6e
Carlos Laurine <carlos.pinho@ucsal.edu.com.br>
2021-03-17 22:11:48 -0300

    Department Entity Class Implemented

---
+++ src/application/MainProgram.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		
+		Department object = new Department(1,"Books");
+		
+		System.out.println(object);
+
+	}
+
+}
